9e51cc92b50304b53a0e009c7b127d95256b6985
Damian Wasilenko <jdequo@gmail.com>
2021-12-29 14:48:54 +0100

    Add me endpoint

--- e-school/src/main/java/com/ztp/eschool/repositories/UserRepository.java
+++ e-school/src/main/java/com/ztp/eschool/repositories/UserRepository.java
@@ -1,8 +1,12 @@
 package com.ztp.eschool.repositories;
 
 import com.ztp.eschool.entities.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByUsername(String username);
+
+    @Query("select u from User u where u.username = ?#{principal?.username}")
+    User me();
 }
