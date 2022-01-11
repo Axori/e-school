1e2ebfcae5331075690e9abc3184b345a8fc4cda
Dawid Taborski <taborski.dawid@gmail.com>
2022-01-11 18:18:52 +0100

    fixing styles and general flow

--- e-school/src/main/js/app.js
+++ e-school/src/main/js/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Link, Navigate, Route, Routes} from "react-router-dom";
 import ReactDOM from 'react-dom';
 import client from './client';
 import {useEffect, useState} from "react";
 import View from "./screens/View/View";
-import AdminDashboard from './screens/View/AdminDashboard';
+import AdminDashboard from './screens/AdminDashboard/AdminDashboard';
+import {USER_ROLES} from "./constants";
+import CreateGroup from "./screens/CreateGroup/CreateGroup";
 
 const App = () => {
     const [user, setUser] = useState();
@@ -15,12 +17,26 @@ const App = () => {
         });
     }, [])
 
-
     return (
         <BrowserRouter>
+            {user?.role === USER_ROLES.ADMIN && <nav className="navbar navbar-expand-lg navbar-light">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <Link to="view" className="nav-link">View Groups</Link>
+                    </li>
+                    <li className="nav-item active">
+                        <Link to="admin" className="nav-link">Admin Panel</Link>
+                    </li>
+                    <li className="nav-item active">
+                        <Link to="admin/create-group" className="nav-link">View Groups</Link>
+                    </li>
+                </ul>
+            </nav>}
             <Routes>
                 <Route path="/view" element={user && <View user={user}/>}/>
-                <Route path="/admin/*" element={user && <AdminDashboard user={user}/>}/>
+                <Route path="/admin" element={user && <AdminDashboard user={user}/>}/>
+                <Route path="/admin/create-group" element={<CreateGroup/>}/>
+                <Route path="/*" element={<Navigate to="/view"/>}/>
             </Routes>
         </BrowserRouter>
     )
